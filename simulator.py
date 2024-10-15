70b946efe22dae7610cf94eb0bfc8ddc568ca174
vp <vp@DESKTOP-FP24GS2>
2024-10-15 23:43:44 +0900

    セット数の決め方を実機に寄せた

--- simulator.py
+++ simulator.py
@@ -1,17 +1,34 @@
 import random as rd
 
 #rushセット数を決める 
-#引数   r:乱数
 #戻り値 セット数
-def set(r):
-    if r <= 7:
-        return 3
-    elif r <= 32:
-        return 2
-    elif r <= 64:
-        return 1
-    else:
-        return 0
+def set():
+    A = 2
+    B = 2
+    C = 1
+    D = 1
+    flg = 0
+    while(True):
+        r = rd.randint(1,100)
+        if r <= 16:
+            A -= 1
+            flg += 1
+        elif r <= 32:
+            B -= 1
+            flg += 1
+        elif r <= 48:
+            C -= 0
+            flg += 1
+        elif r <= 64:
+            D -= 1
+            flg += 1
+        else:#通常
+            flg += 0
+            break
+        
+        if A == 0 or B == 0 or C == 0 or D == 0:
+            break
+    return flg
 
 #rush
 #引数   k:セット数
@@ -39,7 +56,7 @@ for i in range(10):
         n += 1
         if r == 1: #当たり
             r = rd.randint(1,100)
-            out += rush(set(r)) #ラッシュ中の出玉加算
+            out += rush(set()) #ラッシュ中の出玉加算
             break
     res.append([n,out])#結果
 
